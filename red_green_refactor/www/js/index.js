bd54c1b93110544537725a9d425b69da41921530
Zack Bishop <zackbishop@Zacharys-iMac.local>
2013-04-14 11:57:31 -0400

    checking in

--- red_green_refactor/www/js/index.js
+++ red_green_refactor/www/js/index.js
@@ -16,34 +16,30 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var screens = ['red', 'green', 'refactor'];
+document.addEventListener('click', function (evt) {
+    console.log('fired');
+    if (event.target.getAttribute('class') == 'screen') {
+        var oldScreenEle = evt.target;
+        var oldScreen = oldScreenEle.id;
+        var newScreen;
+        var newScreenEle;
 
-        console.log('Received Event: ' + id);
+        screens.forEach(function (screenId, i) {
+            console.log('fired2')
+            if (screenId == oldScreen) {
+                console.log('fired3')
+                if ((i + 1) < screens.length) {
+                    console.log('fired4')
+                    newScreen = screens[i + 1];
+                } else {
+                    newScreen = screens[0];
+                }
+            }
+            newScreenEle = document.getElementById(newScreen);
+        });
+        newScreenEle.style.webkitTransform = 'translate(0px,0px)';
+        oldScreenEle.style.webkitTransform = 'translate(-320px,0px)';
     }
-};
+});
